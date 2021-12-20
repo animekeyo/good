d019bcdb6b70d49462c835ce9b4c66bee7e7d1e3
Syera Chan <keyoanipedia@gmail.com>
2021-12-20 04:51:47 -0800

    aa

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path');
+const fetch = require('cross-fetch');
+const app = express()
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, resp) {
+    resp.render('index');
+})
+app.get('*', function(req, res) {
+    res.status(404).send('what???');
+});
+app.listen(80)
